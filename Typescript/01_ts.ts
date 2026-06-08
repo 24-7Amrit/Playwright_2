6198db2da80f663d8ace8c53890509579b92e3a2
24-7Amrit <247amritraj@gmail.com>
2026-06-08 13:20:58 +0530

    ts

---
+++ Typescript/01_ts.ts
@@ -0,0 +1,18 @@
+// primitive types
+
+let name1:string = "abc";
+//when writing it is saying can not declare
+// and showing some error crosscheck why is the error comming
+let age:number=22;
+let isactive:boolean=true;
+let nothing:null=null;
+let notDefined:undefined=undefined;
+
+let number:number[]=[1,2,3,44,53,44];
+
+let names:Array<string>= ["apne", "llaro"];
+//let name3:Array[string]=["aaas","deded"];
+//why can't we write in above manner
+// since it is showing error ?
+let unknown:unknown = "jelle";
+
