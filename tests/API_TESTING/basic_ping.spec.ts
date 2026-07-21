5c6cd5c81d506169bb1b3bb576531d0a734b83f8
24-7Amrit <247amritraj@gmail.com>
2026-07-21 22:11:49 +0530

    get data

--- tests/API_TESTING/basic_ping.spec.ts
+++ tests/API_TESTING/basic_ping.spec.ts
@@ -4,3 +4,10 @@ test('ping',async ({request})=>{
    const responseData = await request.get('https://restful-booker.herokuapp.com/ping');
    expect(responseData.status()).toBe(201);
 })
+
+test('get all bookings',async ({request})=>{
+   const response = await request.get('https://restful-booker.herokuapp.com/booking');
+   expect(response.status()).toBe(200);
+   const body = await response.json();
+   console.log(body);
+})
